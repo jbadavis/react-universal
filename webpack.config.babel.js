f41a58876823d14d0dafaf9948c99633535581b3
Jack Davis <jxd231@bham.ac.uk>
2017-06-18 16:55:30 +0100

    Initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,26 @@
+module.exports = {
+  output: {
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(sass|scss|css)$/,
+        use: [{
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader",
+          options: {
+            module: true,
+            localIdentName: '[name]__[local]',
+            importLoaders: 1,
+          }
+        },
+        {
+          loader: "sass-loader"
+        }]
+      },
+    ],
+  },
+};
