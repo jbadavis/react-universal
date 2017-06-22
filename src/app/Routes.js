0496f9abf412c64029b3973f30555887f8fb71b0
Jack Davis <jack.davis@akqa.com>
2017-06-22 15:27:33 +0100

    404 error handling

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Error from './pages/Error/Error';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path='/' component={ Home } />
       <Route path='/about' component={ About } />
+      <Route path='*' component= { Error } />
     </Switch>
   );
 };
---
+++ src/app/pages/Error/Error.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <h1>Sorry, the page you're looking for couldn't be found</h1>
+  );
+};
+
+export default Error;
